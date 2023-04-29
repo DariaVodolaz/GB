aff100384dee798793b48b9843ecc4aba098d77f
Daria <dashanovikova59@gmail.com>
2023-04-29 19:11:03 +0300

    Задача 8

---
+++ Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 8
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string result = "";
+
+for (int i = 2; i <= number; i+=2){
+    if ( i % 2 == 0 ){
+        result += $"{i}, ";
+    }
+}
+
+Console.WriteLine(result.Substring(0, result.Length-2));
