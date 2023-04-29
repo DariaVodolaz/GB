e452d3231b91fc177227d519e1aa4417a971c559
Daria <dashanovikova59@gmail.com>
2023-04-29 18:58:04 +0300

    Задача 4

---
+++ Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 4
+Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber){
+    if (firstNumber > thirdNumber){
+        Console.WriteLine($"Большее - {firstNumber}");
+    }
+    else{
+        Console.WriteLine($"Большее - {secondNumber}");
+    }
+}
+else{
+    if (secondNumber > thirdNumber){
+        Console.WriteLine($"Большее - {secondNumber}");
+    }
+    else{
+        Console.WriteLine($"Большее - {thirdNumber}");
+    }
+}
