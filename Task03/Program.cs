9d338e59b07c1a129a988794e256397e769b7c81
Daria <dashanovikova59@gmail.com>
2023-04-29 19:03:36 +0300

    Задача 6

---
+++ Task03/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 6
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number % 2 == 0 ){
+    Console.WriteLine("Четное");
+}
+else{
+    Console.WriteLine("Нечетное");
+}
