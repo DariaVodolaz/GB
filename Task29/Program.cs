a411c9aa6ad137db7df1cc0b3822805b8201be82
Daria <dashanovikova59@gmail.com>
2023-05-28 12:33:01 +0300

    Домашка 4

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29
+int[] GenerateArray(int length, int min, int max){
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++){
+        Random rand = new Random();
+        result[i] = rand.Next(min, max);
+    }
+
+    return result;
+}
+
+int[] arr = GenerateArray(5, 0, 10);
+
+for (int i = 0; i < arr.Length; i++){
+    Console.Write($"{arr[i]}, ");
+}
